5c6557f12311dae3debb0db0e70070a5424debc0
Tiffany Carter <tafby88@gmail.com>
2018-11-15 19:06:19 +0000

    automatically backed up by learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,7 +2,7 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |key,value|
     value.each do |key2, value2|
-      
+      value2.each do |name|
       new_hash[value] = {
         :color => data[:color],
         :gender => data[:gender],
